7663bd51594e4a840a82497314acf696e99d1ef4
sunny <sunnytomar3786@gmail.com>
2022-01-20 23:57:00 +0530

    first

---
+++ 1.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+    vector<int> v;
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++)
+   {int a;
+   cin>>a;
+       v.push_back(a);
+   }
+    /*for(auto it=v.begin();it!=v.end();it++)
+    cout<<(*it);*/
+    for(int c:v)
+    {
+        cout<<c;
+    }
+    return 0;
+
+}
